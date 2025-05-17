f089cf238938e49305f5f0bbee65a421d8ff1129
WTHACD <walejandrocamacho@gmail.com>
2025-05-17 14:50:38 -0600

    Cart solve

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,19 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // 1. Obtener el carrito actual (si no existe, usar arreglo vacío)
+  let cart = getLocalStorage("so-cart") || [];
+
+  // 2. Agregar el nuevo producto
+  cart.push(product);
+
+  // 3. Guardar el nuevo carrito completo
+  setLocalStorage("so-cart", cart);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
